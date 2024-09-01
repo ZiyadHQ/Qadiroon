b0a94777d8b014dd9b34c5576370ae296f70cda4
ZiyadHQ <ziyadhaqbani@gmail.com>
2024-09-02 00:11:13 +0300

    تحديث المتطلبات.

--- lib/universal_widgets/personal_account_widget/user_personal_account.dart
+++ lib/universal_widgets/personal_account_widget/user_personal_account.dart
@@ -1,6 +1,5 @@
 
 import 'dart:io';
-import 'dart:math';
 
 import 'package:camera/camera.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -8,8 +7,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:qadiroon_front_end/simple_alert_widgets.dart';
 import 'package:qadiroon_front_end/styled%20widgets/styled_text.dart';
-import 'package:qadiroon_front_end/universal_widgets/camera_widgets/camera_widget.dart';
 
 Future<String?> uploadImage(XFile imageFile) async
 {
@@ -86,7 +85,7 @@ class _userPersonalAccountScreenState extends State<userPersonalAccountScreen>
         SizedBox(height: 64,),
         TextButton(onPressed: () async {imageFile = await ImagePicker().pickImage(source: ImageSource.gallery)?? imageFile; print("path: ${imageFile?.path?? "image null"}");}, child: Text("TAKE IMAGE")),
         SizedBox(height: 64,),
-        TextButton(onPressed: () async {print("image URL: ${await uploadImage(imageFile)}");}, child: Text("UPLOAD IMAGE")),
+        TextButton(onPressed: () async {simple_alert_showWidget(context, "رفع الصورة لقاعدة البيانات", isDismissible: false); print("image URL: ${await uploadImage(imageFile)}"); Navigator.pop(context);}, child: Text("UPLOAD IMAGE")),
         SizedBox(height: 64,),
         TextButton(onPressed: (){showModalBottomSheet(context: context, builder: (context) => Image.file(File(imageFile!.path)));}, child: Text("SHOW IMAGE")),
         StyledText(text: "${widget.userData!["userType"]}", size: 24, color: Colors.black, fontFamily: "Amiri"),
